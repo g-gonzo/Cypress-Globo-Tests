9b0237a8f813e77a6314e568e37c886d8cf15e05
Gilbert Gonzales <gigonz@extendhealth.com>
2021-06-17 15:18:08 -0600

    redo of header tests

--- Tests/cypress/integration/header.spec.js
+++ Tests/cypress/integration/header.spec.js
@@ -1,84 +1,38 @@
 /// <reference types="cypress" />
 describe('Header displays on all web pages', () => {
     it('header should display on home page', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('.col-md-8 > h1').should('contain', 'THE FUTURE')
-        cy.get(':nth-child(1) > h3').should('contain', 'DARK ENERGY')
-        cy.get('#sessions > :nth-child(3)')
-        expect('#sessions > :nth-child(3)').to.exist
-    })
-        //You Left off here
-    it('header should display on Our story page', () => {
-        cy.get('#hamburger').click
-        cy.get('.nav > .navbar-nav > :nth-child(1) > .nav-link').click
-        cy.get('.banner > img').should.exist
+        cy.visit('http://localhost:3000/index.html')
+        cy.get('header').exists
 
     })
-            // This TEST Should FAIL! no new page available..
+
+    it('header should display on Our Story page', () => {
+        cy.visit('http://localhost:3000/our-story.html')
+        cy.get('header').exists
+
+    })
+
     it('header should display on Brands page', () => {
-        cy.get('#hamburger').click
-        cy.get('.nav > .navbar-nav > :nth-child(2) > .nav-link').click
+        cy.visit('http://localhost:3000/brands.html')
+        cy.get('header').exists
+
     })
 
     it('header should display on Robotics page', () => {
-        cy.get('#hamburger').click
-        cy.get('.nav > .navbar-nav > :nth-child(3) > .nav-link').click
-        expect('h2').to.exist
+        cy.visit('http://localhost:3000/robotics.html')
+        cy.get('header').exists
+
     })
 
     it('header should display on Media page', () => {
-        cy.get('#hamburger').click
-        cy.get('.nav > .navbar-nav > :nth-child(4) > .nav-link').click
-        expect('h2').to.exist
+        cy.visit('http://localhost:3000/media.html')
+        cy.get('header').exists
+
     })
-            // This TEST Should FAIL! no new page available...
+
     it('header should display on Support page', () => {
-        cy.get('#hamburger').click
-        cy.get('.nav > .navbar-nav > :nth-child(5) > .nav-link').click
-        expect('h1').to.exist
-    })
-
-})
-
-describe('Header images test', () => {
-    it('header should have Globo image', () => {
-        cy.get('.logo > a > img').should.exist
-    })
-
-    it('header should have Sign in image', () => {
-        
-
-    })
-
-})
-
-describe('Header Nav links', () => {
-    it('header should have five nav links', () => {
-
-
-    })
-
-})
-
-describe('Header hamburger stack', () => {
-    it('header should display hamburger stack', () => {
-
-
-    })
-
-})
-
-describe('Header search box', () => {
-    it('header should display search box', () => {
-
-
-    })
-
-})
-
-describe('Header close button', () => {
-    it('header should display close button', () => {
-
+        cy.visit('http://localhost:3000/support.html')
+        cy.get('header').exists
 
     })
 
